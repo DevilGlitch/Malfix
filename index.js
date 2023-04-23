a037ed450c1df1c7f44dffd8ddff61fab1992e61
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-04-22 22:05:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
 const token = process.env.BOT_TOKEN;
+const btO = 883383632566321202;
+const pfx = '>'
 const guildIds = process.env.GUILD_IDS.split(',');
 const applicationId = process.env.APPLICATION_ID;
 
@@ -19,14 +21,26 @@ const commands = [
       {
         name: 'user',
         description: 'The user to kick',
-        type: 'USER',
+        type: '6',
         required: true,
       },
       {
         name: 'reason',
         description: 'The reason for kicking the user',
-        type: 'STRING',
+        type: '3',
         required: false,
+      },
+    ],
+  },
+  {
+    name: 'inv',
+    description: 'Joins the server using the provided invite link',
+    options: [
+      {
+        name: 'link',
+        description: 'The invite link',
+        type: '3',
+        required: true,
       },
     ],
   },
@@ -62,7 +76,8 @@ client.on('interactionCreate', async interaction => {
 
   if (commandName === 'ping') {
     await interaction.reply('Pong!');
-  } else if (commandName === 'kick') {
+  } 
+   if (commandName === 'kick') {
     const user = options.getUser('user');
     const reason = options.getString('reason') || 'No reason provided';
     const member = interaction.guild.members.cache.get(user.id);
@@ -77,6 +92,28 @@ client.on('interactionCreate', async interaction => {
 
     await member.kick(reason);
     await interaction.reply(`${user.tag} has been kicked by ${interaction.user.tag} because: ${reason}`);
+  } else if (commandName === 'inv') {
+    const link = options.getString('link');
+    const invite = await client.invites.create(link).catch(() => null);
+
+    if (!invite) {
+      return interaction.reply('Invalid invite link.');
+    }
+
+    if (!guild) {
+      return interaction.reply('Unable to join the server.');
+    }
+
+      
+    const member = guild.members.cache.get(interaction.user.id);
+
+    if (!member) {
+      return interaction.reply('You must be in the server to use this command.');
+    }
+
+    await member.send(`Joining server: ${guild.name}`);
+    await invite.accept();
+    await interaction.reply(`Joined server: ${guild.name}`);
   }
 });
 
